fee11ebf4f652e691cea0f44ba2810813fe04577
tabatak <hamamoto.kazuto@gmail.com>
2017-06-26 21:15:51 +0900

    2.6

---
+++ ch2/cf/main.go
@@ -0,0 +1,22 @@
+// Cf converts its numeric argument to Celsius and Fehrenheit.
+package main
+
+import (
+	"fmt"
+	"gopl/ch2/tempconv"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
---
+++ ch2/ex02/unitconv/unitconv_test.go
@@ -0,0 +1,33 @@
+package unitconv_test
+
+import (
+	"fmt"
+	"gopl/ch2/ex02/unitconv"
+	"testing"
+)
+
+func TestCToF(t *testing.T) {
+	if unitconv.CToF(unitconv.BoilingC) != 212.0 {
+		t.Error(fmt.Sprint(unitconv.CToF(unitconv.BoilingC)))
+	}
+}
+
+func TestKToC(t *testing.T) {
+	if unitconv.KToC(0) != unitconv.AbsoluteZeroC {
+		t.Error(fmt.Sprint(unitconv.KToC(0)))
+	}
+}
+
+func TestCToK(t *testing.T) {
+	if unitconv.CToK(0) != -unitconv.Kelvin(unitconv.AbsoluteZeroC) {
+		t.Error(fmt.Sprint(unitconv.CToK(0)))
+	}
+}
+
+func TestFToK(t *testing.T) {
+	expected := unitconv.FToC(0) - unitconv.AbsoluteZeroC
+
+	if unitconv.FToK(0) != unitconv.Kelvin(expected) {
+		t.Error(fmt.Sprint(unitconv.FToK(0)))
+	}
+}
